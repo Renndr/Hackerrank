7ab42ea22816a5e77319ed0c310ceb83b78abe4d
Renndr <manueldejbernabel@gmail.com>
2024-01-02 23:46:33 -0400

    add java challenges

---
+++ Java/Java Datatypes/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    
+    public static void main(String[] args) {
+        double h = Math.pow(2, 31);
+        int q = 2147483647;
+        long p = 1223372039;
+        System.out.println(q);
+    }
+}
