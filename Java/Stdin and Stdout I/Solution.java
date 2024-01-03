564d67286edc749eb9bbc61782b92cb6830096a3
Renndr <manueldejbernabel@gmail.com>
2024-01-02 23:55:22 -0400

    update challenge

--- Java/Stdin and Stdout I/Solution.java
+++ Java/Stdin and Stdout I/Solution.java
@@ -1,14 +1,18 @@
+import java.io.*;
 import java.util.*;
 
 public class Solution {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String myString = scanner.next();
-        int myInt = scanner.nextInt();
+        int one = scanner.nextInt();
+        int two = scanner.nextInt();
+        int three = scanner.nextInt();
         scanner.close();
 
-        System.out.println("myString is: " + myString);
-        System.out.println("myInt is: " + myInt);
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        
     }
 }
