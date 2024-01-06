faca79275aaf657fdf522e042ccfd04d0b5df161
Renndr <manueldejbernabel@gmail.com>
2024-01-05 23:15:32 -0400

    add day 1 code

---
+++ Tutorials/30 Days of Code/Python/Day 1 Data Types/Day1.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    n2 = float(input().strip())
+    s2 = (input().strip())
+    
+    print(i + n)
+    print(d + n2)
+    print(s + s2)
